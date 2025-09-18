5f82e31a1a9c91a99f3e4a7c4f3bdf22535745fd
thialms <thiiago.allmeida44@gmail.com>
2025-09-18 09:18:17 -0300

    feat: add main content

--- src/main/java/br/edu/fatecpg/atividadepokemon/AtividadePokemonApplication.java
+++ src/main/java/br/edu/fatecpg/atividadepokemon/AtividadePokemonApplication.java
@@ -1,13 +1,40 @@
 package br.edu.fatecpg.atividadepokemon;
 
+import br.edu.fatecpg.atividadepokemon.client.ConsomeAPI;
+import br.edu.fatecpg.atividadepokemon.model.DadosPokemon;
+import br.edu.fatecpg.atividadepokemon.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class AtividadePokemonApplication {
+public class AtividadePokemonApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AtividadePokemonApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o pokemon: ");
+        String pokemon = sc.nextLine().toLowerCase();
+
+        String url = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+
+        ConsomeAPI api = new ConsomeAPI();
+        String json = api.obterDados(url);
+
+        ConverteDados converteDados = new ConverteDados();
+        DadosPokemon dadosPokemon = converteDados.obterDados(json, DadosPokemon.class);
+
+        System.out.println("Nome: " + dadosPokemon.nome());
+        System.out.println("Altura: " + dadosPokemon.altura());
+        System.out.println("Peso: " + dadosPokemon.peso());
+
+        sc.close();
+    }
 }
