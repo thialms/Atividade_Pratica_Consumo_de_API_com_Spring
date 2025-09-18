162f4b1a7f68284c3819e12336089e15ad36e696
thialms <thiiago.allmeida44@gmail.com>
2025-09-18 09:00:26 -0300

    project init

---
+++ src/main/java/br/edu/fatecpg/atividadepokemon/AtividadePokemonApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.atividadepokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePokemonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadePokemonApplication.class, args);
+    }
+
+}
